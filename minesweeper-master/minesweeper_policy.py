6efcbbcd8d70c4a7e2c1f29c652e61ed5342ad1e
Alisher Tortay <t.b.alisher@gmail.com>
2016-11-13 01:00:53 +0900

    Add files via upload

--- minesweeper-master/minesweeper_policy.py
+++ minesweeper-master/minesweeper_policy.py
@@ -4,38 +4,49 @@ import numpy as np
 from game import *
 from collections import deque
 
-size = 4
-mines = 4
+size = 8
+mines = 8
 
 env_name = 'Minesweeper'
 env = Game(size, size, mines)
 
 sess = tf.Session()
-optimizer = tf.train.RMSPropOptimizer(learning_rate=0.0001, decay=0.9)
+optimizer = tf.train.RMSPropOptimizer(learning_rate=0.000001, decay=0.9)
 writer = tf.train.SummaryWriter("/tmp/{}-experiment-1".format(env_name))
 
-state_dim = size*size
+state_dim = size*size*10
 num_actions = size*size
+
+def toHot(inputState):
+	temp = []
+	s = np.array([])
+	for i in range(inputState.shape[1]):
+		sample = np.zeros(10)
+		sample[int(inputState[0,i])] = 1.0
+		temp.append(sample)
+	return np.asmatrix(np.append(s, temp))
 
 def policy_network(states):
 	# define policy neural network
-	W1 = tf.get_variable("W1", [state_dim, 100], initializer=tf.random_normal_initializer())
-	b1 = tf.get_variable("b1", [100], initializer=tf.constant_initializer(0))
+	W1 = tf.get_variable("W1", [state_dim, 1024], initializer=tf.random_normal_initializer())
+	b1 = tf.get_variable("b1", [1024], initializer=tf.constant_initializer(0))
 	h1 = tf.nn.tanh(tf.matmul(states, W1) + b1)
-	W2 = tf.get_variable("W2", [100, num_actions], initializer=tf.random_normal_initializer(stddev=0.1))
+	W2 = tf.get_variable("W2", [1024, num_actions], initializer=tf.random_normal_initializer(stddev=0.1))
 	b2 = tf.get_variable("b2", [num_actions], initializer=tf.constant_initializer(0))
 	p = tf.matmul(h1, W2) + b2
 	return p
 
 pg_reinforce = PolicyGradientREINFORCE(sess, optimizer, policy_network, state_dim, num_actions, summary_writer = writer)
 
-MAX_EPISODES = 5000
-MAX_STEPS = 20
+MAX_EPISODES = 10000
+MAX_STEPS = 10
 
 episode_history = deque(maxlen=100)
 for i_episode in xrange(MAX_EPISODES):
 	# initialize
 	state = env.reset()
+	state, _, _ = env.open((0,0))
+	state = toHot(state)
 	total_rewards = 0
 
 	done = False
@@ -47,8 +58,11 @@ for i_episode in xrange(MAX_EPISODES):
 		total_rewards += reward
 		pg_reinforce.storeRollout(state, action, reward)
 
-		state = next_state
-		if done: break
+		
+		if done: 
+			break
+		else:
+			state = toHot(next_state)
 
 	pg_reinforce.updateModel()
 
@@ -59,3 +73,4 @@ for i_episode in xrange(MAX_EPISODES):
 	print("Finished after {} timesteps".format(t+1))
 	print("Reward for this episode: {}".format(total_rewards))
 	print("Average reward for last 100 episodes: {}".format(mean_rewards))
+
